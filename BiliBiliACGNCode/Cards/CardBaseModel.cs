49d6f80a5709da9cfedc1a6a3c222155b820ff90
imwheat <62542063+imwheat@users.noreply.github.com>
2026-03-31 10:17:06 +0800

    update 更新默认图

--- BiliBiliACGNCode/Cards/CardBaseModel.cs
+++ BiliBiliACGNCode/Cards/CardBaseModel.cs
@@ -7,6 +7,7 @@
 using BaseLib.Abstracts;
 using BaseLib.Extensions;
 using BiliBiliACGN.BiliBiliACGNCode.Extensions;
+using Godot;
 using MegaCrit.Sts2.Core.Entities.Cards;
 
 namespace BiliBiliACGN.BiliBiliACGNCode.Cards;
@@ -16,15 +17,37 @@ public abstract class CardBaseModel : CustomCardModel
     //Image size:
     //Normal art: 1000x760 (Using 500x380 should also work, it will simply be scaled.)
     //Full art: 606x852
-    public override string CustomPortraitPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigCardImagePath();
+    public override string CustomPortraitPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigCardImagePath();
+            return ResourceLoader.Exists(path) ? path : "none.png".BigCardImagePath();
+        }
+    }
     
     //Smaller variants of card images for efficiency:
     //Smaller variant of fullart: 250x350
     //Smaller variant of normalart: 250x190
     
     //Uses card_portraits/card_name.png as image path. These should be smaller images.
-    public override string PortraitPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".CardImagePath();
-    public override string BetaPortraitPath => $"beta/{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".CardImagePath();
+    public override string PortraitPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".CardImagePath();
+            return ResourceLoader.Exists(path) ? path : "none.png".CardImagePath();
+        }
+    }
+
+    public override string BetaPortraitPath
+    {
+        get
+        {
+            var path = $"beta/{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".CardImagePath();
+            return ResourceLoader.Exists(path) ? path : "none.png".CardImagePath();
+        }
+    }
 
     public CardBaseModel(int canonicalEnergyCost, CardType type, CardRarity rarity, TargetType targetType, bool shouldShowInCardLibrary = true) : base(canonicalEnergyCost, type, rarity, targetType, shouldShowInCardLibrary)
     {
