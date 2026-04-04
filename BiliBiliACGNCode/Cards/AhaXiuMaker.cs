c2cc09e2fae9213da345295c4a80471c0a8a18c8
imwheat <62542063+imwheat@users.noreply.github.com>
2026-04-04 12:52:51 +0800

    update 调整卡牌
    - 啊哈修Maker改为X攻击牌

--- BiliBiliACGNCode/Cards/AhaXiuMaker.cs
+++ BiliBiliACGNCode/Cards/AhaXiuMaker.cs
@@ -2,7 +2,7 @@
 //* 文件：AhaXiuMaker(啊哈！修maker)
 //* 作者：wheat
 //* 创建时间：2026/04/03
-//* 描述：造成{Damage:diff()}点伤害，获得{Block:diff()}点格挡。
+//* 描述：对所有敌人造成{Damage:diff()}点伤害X次。
 //*******************************************************
 
 using BaseLib.Utils;
@@ -12,6 +12,13 @@ using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
 using BiliBiliACGN.BiliBiliACGNCode.Cards.CardPool;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Saves;
+using MegaCrit.Sts2.Core.Settings;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
+using MegaCrit.Sts2.Core.Nodes;
+using Godot;
 
 namespace BiliBiliACGN.BiliBiliACGNCode.Cards;
 
@@ -19,7 +26,9 @@ namespace BiliBiliACGN.BiliBiliACGNCode.Cards;
 public sealed class AhaXiuMaker : CardBaseModel
 {
     #region 卡牌属性配置
-    private const int energyCost = 1;
+    private const int energyCost = -1;
+    protected override bool HasEnergyCostX => true;
+
     private const CardType type = CardType.Attack;
     private const CardRarity rarity = CardRarity.Uncommon;
     private const TargetType targetType = TargetType.AnyEnemy;
@@ -28,7 +37,6 @@ public sealed class AhaXiuMaker : CardBaseModel
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
         new DamageVar(7m, ValueProp.Move),
-        new BlockVar(7m, ValueProp.Move)
     ];
 
     public AhaXiuMaker() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary) { }
@@ -37,17 +45,29 @@ public sealed class AhaXiuMaker : CardBaseModel
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        // 造成伤害并获得格挡
+        // 动画效果
+        int num = ResolveEnergyXValue();
+        if(num <= 0) return;
+
+        // 播放动画效果
+		Color color = new Color("FFFFFF80");
+		double num2 = ((SaveManager.Instance.PrefsSave.FastMode == FastModeType.Fast) ? 0.2 : 0.3);
+		NCombatRoom.Instance?.CombatVfxContainer.AddChildSafely(NHorizontalLinesVfx.Create(color, 0.8 + (double)Mathf.Min(8, num) * num2));
+		SfxCmd.Play("event:/sfx/characters/ironclad/ironclad_whirlwind");
+		NRun.Instance?.GlobalUi.AddChildSafely(NSmokyVignetteVfx.Create(color, color));
+        // 造成伤害 X 次
         await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue)
             .FromCard(this)
-            .Targeting(cardPlay.Target)
+            .TargetingAllOpponents(base.CombatState)
+            .WithHitFx("vfx/vfx_giant_horizontal_slash")
+            .WithHitCount(num)
             .Execute(choiceContext);
-        await CreatureCmd.GainBlock(base.Owner.Creature, base.DynamicVars.Block.BaseValue, base.DynamicVars.Block.Props, cardPlay);
     }
 
     protected override void OnUpgrade()
     {
-        base.DynamicVars["Damage"].UpgradeValueBy(2m);
-        base.DynamicVars["Block"].UpgradeValueBy(2m);
+        // 升级效果 伤害增加1点，获得[gold]有一说一[/gold]
+        base.DynamicVars["Damage"].UpgradeValueBy(1m);
+        base.AddKeyword(CustomKeyWords.YYSY);
     }
 }
