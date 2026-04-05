55a69d2654dbde0644526853493625fffe1d55c8
imwheat <62542063+imwheat@users.noreply.github.com>
2026-04-05 19:05:25 +0800

    fix 修复角色商店加载BUG

---
+++ BiliBiliACGNCode/Nodes/SNMerchantCharacter.cs
@@ -0,0 +1,14 @@
+//****************** 代码文件申明 ***********************
+//* NMerchantCharacter
+//* 作者：wheat
+//* 创建时间：2026/04/05 18:43:00 星期三
+//* 描述：NPC商人角色
+//*******************************************************
+
+using MegaCrit.Sts2.Core.Nodes.Screens.Shops;
+
+namespace BiliBiliACGN.BiliBiliACGNCode.Nodes;
+
+internal partial class SNMerchantCharacter : NMerchantCharacter
+{
+}
