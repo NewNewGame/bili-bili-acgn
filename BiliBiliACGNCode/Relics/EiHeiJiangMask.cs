e1a1873df82a4ffb990a5b94d633a27931faeff7
imwheat <62542063+imwheat@users.noreply.github.com>
2026-04-14 21:05:04 +0800

    update 更新泛式初始遗物

--- BiliBiliACGNCode/Relics/EiHeiJiangMask.cs
+++ BiliBiliACGNCode/Relics/EiHeiJiangMask.cs
@@ -6,7 +6,12 @@
 
 using BaseLib.Utils;
 using BiliBiliACGN.BiliBiliACGNCode.Core.Commands;
+using BiliBiliACGN.BiliBiliACGNCode.Core.Models.Orbs;
+using BiliBiliACGN.BiliBiliACGNCode.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.Models.RelicPools;
@@ -18,18 +23,36 @@ namespace BiliBiliACGN.BiliBiliACGNCode.Relics;
 public sealed class EiHeiJiangMask : RelicBaseModel
 {
     public override RelicRarity Rarity => RelicRarity.Starter;
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new DynamicVar("Power", 3)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+		new DynamicVar("Power", 3), 
+		new DynamicVar("AttackOrb", 1),
+		new DynamicVar("Hp", 10m),
+	];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+		HoverTipFactory.FromPower<StrengthPower>(),
+		HoverTipFactory.FromOrb<AttackOrb>()
+	];
 
     public override async Task BeforeCombatStart()
 	{
 		await SummonPet();
 	}
-
+	public override async Task AfterSideTurnStart(CombatSide side, CombatState combatState)
+	{
+		// 回合开始时，召唤充能球
+		if (side == base.Owner.Creature.Side && combatState.RoundNumber <= 1)
+		{
+			for (int i = 0; (decimal)i < base.DynamicVars["AttackOrb"].BaseValue; i++)
+			{
+				await OrbCmd.Channel<AttackOrb>(CombatUtils.GetTemporaryPlayerChoiceContext(), base.Owner);
+			}
+		}
+	}
 	private async Task SummonPet()
 	{
 		var daughter = await DaughterCmd.SummonDaughter(base.Owner.Creature);
-		daughter.SetMaxHpInternal(11);
-		daughter.SetCurrentHpInternal(11);
+		daughter.SetMaxHpInternal(base.DynamicVars["Hp"].BaseValue + 1);
+		daughter.SetCurrentHpInternal(base.DynamicVars["Hp"].BaseValue + 1);
 		await DaughterCmd.ApplyPower<StrengthPower>(base.Owner.Creature, base.DynamicVars["Power"].BaseValue, null);
 	}
 
--- BiliBiliACGNCode/Relics/WindRangerMask.cs
+++ BiliBiliACGNCode/Relics/WindRangerMask.cs
@@ -7,8 +7,13 @@
 
 using BaseLib.Utils;
 using BiliBiliACGN.BiliBiliACGNCode.Core.Commands;
+using BiliBiliACGN.BiliBiliACGNCode.Core.Models.Orbs;
 using BiliBiliACGN.BiliBiliACGNCode.Relics.RelicPool;
+using BiliBiliACGN.BiliBiliACGNCode.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.Powers;
 
@@ -19,16 +24,38 @@ namespace BiliBiliACGN.BiliBiliACGNCode.Relics;
 public sealed class WindRangerMask : RelicBaseModel
 {
     public override RelicRarity Rarity => RelicRarity.Starter;
-	protected override IEnumerable<DynamicVar> CanonicalVars => [new DynamicVar("Power", 5m)];
+	protected override IEnumerable<DynamicVar> CanonicalVars => [
+		new DynamicVar("Power", 5m), 
+		new DynamicVar("AttackOrb", 2),
+		new DynamicVar("Hp", 15m),
+	];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+		HoverTipFactory.FromPower<StrengthPower>(),
+		HoverTipFactory.FromOrb<AttackOrb>()
+	];
     public override async Task BeforeCombatStart()
 	{
 		await SummonPet();
 	}
+	public override async Task AfterSideTurnStart(CombatSide side, CombatState combatState)
+	{
+		// 回合开始时，召唤充能球
+		if (side == base.Owner.Creature.Side && combatState.RoundNumber <= 1)
+		{
+			for (int i = 0; (decimal)i < base.DynamicVars["AttackOrb"].BaseValue; i++)
+			{
+				await OrbCmd.Channel<AttackOrb>(CombatUtils.GetTemporaryPlayerChoiceContext(), base.Owner);
+			}
+		}
+	}
 	private async Task SummonPet()
 	{
+		// 召唤女儿
 		var daughter = await DaughterCmd.SummonDaughter(base.Owner.Creature);
-		daughter.SetMaxHpInternal(16);
-		daughter.SetCurrentHpInternal(16);
+		// 设置女儿的HP
+		daughter.SetMaxHpInternal(base.DynamicVars["Hp"].BaseValue + 1);
+		daughter.SetCurrentHpInternal(base.DynamicVars["Hp"].BaseValue + 1);
+		// 赋予女儿力量
 		await DaughterCmd.ApplyPower<StrengthPower>(base.Owner.Creature, base.DynamicVars["Power"].BaseValue, null);
 	}
 
