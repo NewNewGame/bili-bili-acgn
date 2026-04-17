ccd363023957857e16cd0017d53cc9e0ddab3e41
imwheat <62542063+imwheat@users.noreply.github.com>
2026-04-17 19:04:19 +0800

    update 死亡轮回遗物调整

--- BiliBiliACGNCode/Relics/DeathRebirthCycle.cs
+++ BiliBiliACGNCode/Relics/DeathRebirthCycle.cs
@@ -2,12 +2,13 @@
 //* 文件：DeathRebirthCycle(死亡轮回)
 //* 作者：wheat
 //* 创建时间：2026/04/06
-//* 描述：死亡后满血复活一次。
+//* 描述：死亡后复活一次，复活后回复60%的生命值。
 //*******************************************************
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.RelicPools;
 using MegaCrit.Sts2.Core.Saves.Runs;
 
@@ -20,6 +21,7 @@ public sealed class DeathRebirthCycle : RelicBaseModel
     private bool _wasUsed;
 
 	public override bool IsUsedUp => _wasUsed;
+	protected override IEnumerable<DynamicVar> CanonicalVars => [new DynamicVar("HealAmount", 60m)];
 
 	[SavedProperty]
 	public bool BILIBILIACGN_DRC_WasUsed
@@ -56,7 +58,7 @@ public sealed class DeathRebirthCycle : RelicBaseModel
 	{
 		Flash();
 		BILIBILIACGN_DRC_WasUsed = true;
-		decimal amount = Math.Max(1m, creature.MaxHp);
+		decimal amount = Math.Max(1m, creature.MaxHp * base.DynamicVars["HealAmount"].BaseValue / 100m);
 		await CreatureCmd.Heal(creature, amount);
 	}
 }
