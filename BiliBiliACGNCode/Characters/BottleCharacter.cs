68de80eb6b240c454a1b8c3eb6362b7a874bbc1c
imwheat <62542063+imwheat@users.noreply.github.com>
2026-04-18 08:03:20 +0800

    update 更新牛角色颜色

--- BiliBiliACGNCode/Characters/BottleCharacter.cs
+++ BiliBiliACGNCode/Characters/BottleCharacter.cs
@@ -23,6 +23,9 @@ public sealed class BottleCharacter : PlaceholderCharacterModel
 {
 	public override string PlaceholderID => "bottle";
 	public override Color NameColor => new Color("789ccd");
+    public override Color MapDrawingColor => new Color("789ccd");
+    public override Color DialogueColor => new Color("789ccd");
+    public override Color EnergyLabelOutlineColor => new Color("184788");
 	public override CharacterGender Gender => CharacterGender.Masculine;
 	public override int StartingHp => 80;
 	public override float CastAnimDelay => 0.25f;
