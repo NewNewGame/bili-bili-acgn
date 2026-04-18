04a97839086150d44c3b1442f6d6e3b3afb5eae0
imwheat <62542063+imwheat@users.noreply.github.com>
2026-04-18 10:54:11 +0800

    fix 修复青眼白龙移除后反复获得金钱的bug

--- BiliBiliACGNCode/Relics/PureGoldCard.cs
+++ BiliBiliACGNCode/Relics/PureGoldCard.cs
@@ -11,6 +11,7 @@ using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Logging;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.RelicPools;
 using MegaCrit.Sts2.Core.Rooms;
@@ -58,7 +59,6 @@ public sealed class PureGoldCard : RelicBaseModel
             base.Status = RelicStatus.Disabled;
             // 移除所有的青眼白龙
             var qingYanBlueEyesWhiteDragon = base.Owner.Deck.Cards.Where(card => card is BlueEyesWhiteDragon).ToList();
-            await PlayerCmd.GainGold(870m * qingYanBlueEyesWhiteDragon.Count, base.Owner);
             await CardPileCmd.RemoveFromDeck(qingYanBlueEyesWhiteDragon);
             BILIBILIACGN_PGC_WasUsed = true;
         }
