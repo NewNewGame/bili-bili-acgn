9177e246979b4f559bf9b70ebbedd90047941559
imwheat <62542063+imwheat@users.noreply.github.com>
2026-04-19 18:41:47 +0800

    update 重做中二笔记

--- BiliBiliACGNCode/Cards/ChuunibyouNote.cs
+++ BiliBiliACGNCode/Cards/ChuunibyouNote.cs
@@ -2,7 +2,7 @@
 //* 文件：ChuunibyouNote(中二笔记)
 //* 作者：wheat
 //* 创建时间：2026/04/08
-//* 描述：每当你打出能力牌时，抽2张牌。
+//* 描述：给予10层病态。每有10层病态，额外给予3层病态。
 //*******************************************************
 
 using BaseLib.Utils;
@@ -11,6 +11,7 @@ using BiliBiliACGN.BiliBiliACGNCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 
 namespace BiliBiliACGN.BiliBiliACGNCode.Cards;
@@ -19,14 +20,17 @@ namespace BiliBiliACGN.BiliBiliACGNCode.Cards;
 public sealed class ChuunibyouNote : CardBaseModel
 {
     private const int energyCost = 1;
-    private const CardType type = CardType.Power;
+    private const CardType type = CardType.Skill;
     private const CardRarity rarity = CardRarity.Rare;
-    private const TargetType targetType = TargetType.Self;
+    private const TargetType targetType = TargetType.AnyEnemy;
     private const bool shouldShowInCardLibrary = true;
-
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<MorbidPower>()];
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new CardsVar(2),
+        new CalculationBaseVar(10m),
+        new CalculationExtraVar(3m),
+        new CalculatedVar("CalculatedMorbid").WithMultiplier((_, target) => 
+        target.GetPowerAmount<MorbidPower>()/10m)
     ];
 
     public ChuunibyouNote() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary) { }
@@ -34,13 +38,13 @@ public sealed class ChuunibyouNote : CardBaseModel
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
         await CreatureCmd.TriggerAnim(base.Owner.Creature, "Cast", base.Owner.Character.CastAnimDelay);
-        // 施加能力：每当你打出能力牌时，抽牌
-        await PowerCmd.Apply<ChuunibyouNotePower>(base.Owner.Creature, base.DynamicVars["Cards"].BaseValue, base.Owner.Creature, this);
+        // 施加能力：给予10层病态。每有10层病态，额外给予3层病态。
+        await PowerCmd.Apply<MorbidPower>(cardPlay.Target, ((CalculatedVar)base.DynamicVars["CalculatedMorbid"]).Calculate(cardPlay.Target), base.Owner.Creature, this);
     }
 
     protected override void OnUpgrade()
     {
-        // 升级后添加固有
-        base.AddKeyword(CardKeyword.Innate);
+        base.DynamicVars["CalculationBase"].UpgradeValueBy(2m);
+        base.DynamicVars["CalculationExtra"].UpgradeValueBy(1m);
     }
 }
