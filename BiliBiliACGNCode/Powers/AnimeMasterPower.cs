16a87ab2fbe0cfe01ea21ce6c9d1b731ad99b922
imwheat <62542063+imwheat@users.noreply.github.com>
2026-04-25 18:07:01 +0800

    Reapply "update 重做动漫高手Power"
    This reverts commit 13cdaece9a5adbd0c736bd9acdcfb068c9db5c91.

--- BiliBiliACGNCode/Powers/AnimeMasterPower.cs
+++ BiliBiliACGNCode/Powers/AnimeMasterPower.cs
@@ -2,19 +2,10 @@
 //* 文件：AnimeMasterPower(动漫高手)
 //* 作者：wheat
 //* 创建时间：2026/04/08
-//* 描述：每当你激发充能球时，给予女儿力量。
+//* 描述：女儿现在会攻击所有敌人。
 //*******************************************************
 
-using BiliBiliACGN.BiliBiliACGNCode.Core.Commands;
-using BiliBiliACGN.BiliBiliACGNCode.Core.Models.Monsters;
-using BiliBiliACGN.BiliBiliACGNCode.Utils;
-using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
-using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.HoverTips;
-using MegaCrit.Sts2.Core.Models;
-using MegaCrit.Sts2.Core.Models.Powers;
 
 namespace BiliBiliACGN.BiliBiliACGNCode.Powers;
 
@@ -22,15 +13,4 @@ public sealed class AnimeMasterPower : PowerBaseModel
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(StaticHoverTip.Evoke), HoverTipFactory.FromPower<StrengthPower>()];
-
-    public override async Task AfterOrbEvoked(PlayerChoiceContext choiceContext, OrbModel orb, IEnumerable<Creature> targets)
-    {
-        if(orb.Owner.Creature != base.Owner) return;
-        var daughter = base.Owner.GetDaughter();
-        if(daughter == null) return;
-        // 给予女儿力量
-        await PowerCmd.Apply<StrengthPower>(daughter, Amount, base.Owner, null);
-    }
-
 }
