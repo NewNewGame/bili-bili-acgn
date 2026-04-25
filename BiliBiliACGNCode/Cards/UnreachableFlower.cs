0d487a89f8965375f07b8886bf60443f71ae9a3d
imwheat <62542063+imwheat@users.noreply.github.com>
2026-04-25 17:08:24 +0800

    update 重做高岭之花

--- BiliBiliACGNCode/Cards/UnreachableFlower.cs
+++ BiliBiliACGNCode/Cards/UnreachableFlower.cs
@@ -2,17 +2,17 @@
 //* 文件：UnreachableFlower(高岭之花)
 //* 作者：wheat
 //* 创建时间：2026/04/11
-//* 描述：给予敌人6/9层病态，抽2/3张牌。
+//* 描述：造成{Damage:diff()}点伤害。抽{Cards:diff()}张牌。下回合抽{Cards:diff()}张牌。
 //*******************************************************
 
 using BaseLib.Utils;
 using BiliBiliACGN.BiliBiliACGNCode.Cards.CardPool;
-using BiliBiliACGN.BiliBiliACGNCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace BiliBiliACGN.BiliBiliACGNCode.Cards;
 
@@ -20,31 +20,32 @@ namespace BiliBiliACGN.BiliBiliACGNCode.Cards;
 public sealed class UnreachableFlower : CardBaseModel
 {
     private const int energyCost = 1;
-    private const CardType type = CardType.Skill;
+    private const CardType type = CardType.Attack;
     private const CardRarity rarity = CardRarity.Uncommon;
     private const TargetType targetType = TargetType.AnyEnemy;
     private const bool shouldShowInCardLibrary = true;
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<MorbidPower>()];
-
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new DynamicVar("Morbid", 8m),
-        new CardsVar(2),
+        new DamageVar(9m, ValueProp.Move),
+        new CardsVar(1),
     ];
 
     public UnreachableFlower() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary) { }
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        if(base.CombatState == null) return;
-        // 给予敌人6/9层病态
-        await PowerCmd.Apply<MorbidPower>(cardPlay.Target, base.DynamicVars["Morbid"].BaseValue, base.Owner.Creature, this);
-        // 抽2/3张牌
+        // 造成伤害
+        await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue)
+            .FromCard(this)
+            .Targeting(cardPlay.Target)
+            .Execute(choiceContext);
+        // 抽1张牌
         await CardPileCmd.Draw(choiceContext, base.DynamicVars.Cards.BaseValue, base.Owner);
+        // 下回合抽1张牌
+        await PowerCmd.Apply<DrawCardsNextTurnPower>(base.Owner.Creature, base.DynamicVars.Cards.BaseValue, base.Owner.Creature, this);
     }
 
     protected override void OnUpgrade() { 
-        base.DynamicVars["Morbid"].UpgradeValueBy(3m);
-        base.DynamicVars["Cards"].UpgradeValueBy(1m);
+        base.DynamicVars["Damage"].UpgradeValueBy(4m);
     }
 }
