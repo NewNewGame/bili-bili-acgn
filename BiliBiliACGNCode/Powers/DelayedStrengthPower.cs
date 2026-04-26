80cc100cd9ccf2ba0ea74c663848354889d8980e
imwheat <62542063+imwheat@users.noreply.github.com>
2026-04-26 21:22:17 +0800

    Update DelayedStrengthPower.cs

--- BiliBiliACGNCode/Powers/DelayedStrengthPower.cs
+++ BiliBiliACGNCode/Powers/DelayedStrengthPower.cs
@@ -7,9 +7,7 @@
 
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Powers;
-using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Models.Powers;
 
