6b9ecbed067272828f83e24740dd81fea52c034d
imwheat <62542063+imwheat@users.noreply.github.com>
2026-04-26 21:32:22 +0800

    fix 修复临时添加最大生命值bug

--- BiliBiliACGNCode/Powers/AddMaxHpTempPower.cs
+++ BiliBiliACGNCode/Powers/AddMaxHpTempPower.cs
@@ -24,7 +24,7 @@ public sealed class AddMaxHpTempPower : PowerBaseModel
     public override async Task AfterPowerAmountChanged(PowerModel power, decimal amount, Creature? applier, CardModel? cardSource)
     {
         // 如果不是本能力，对象不是自己，则返回
-        if(power != this && power.Owner != base.Owner) return;
+        if(power != this || power.Owner != base.Owner) return;
 
         // 如果数量小于等于0，则返回
         if(amount <= 0) return;
