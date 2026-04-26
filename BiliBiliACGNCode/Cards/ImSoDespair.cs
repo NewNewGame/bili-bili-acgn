9c1fb28911f24dd5653a86edd1a8f932744e95f2
imwheat <62542063+imwheat@users.noreply.github.com>
2026-04-26 13:24:28 +0800

    update 重做我好绝望啊

--- BiliBiliACGNCode/Cards/ImSoDespair.cs
+++ BiliBiliACGNCode/Cards/ImSoDespair.cs
@@ -2,47 +2,37 @@
 //* 文件：ImSoDespair(我好绝望啊)
 //* 作者：wheat
 //* 创建时间：2026/04/08
-//* 描述：获得5/9点格挡。本回合，拥有病态的敌人对你造成伤害减半。
+//* 描述：丢弃你的所有[gold]手牌[/gold]。\n然后抽取相同数量的牌。
 //*******************************************************
 
 using BaseLib.Utils;
 using BiliBiliACGN.BiliBiliACGNCode.Cards.CardPool;
-using BiliBiliACGN.BiliBiliACGNCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.HoverTips;
-using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.ValueProps;
 
 namespace BiliBiliACGN.BiliBiliACGNCode.Cards;
 
 [Pool(typeof(FunShikiCardPool))]
 public sealed class ImSoDespair : CardBaseModel
 {
-    private const int energyCost = 1;
+    private const int energyCost = 0;
     private const CardType type = CardType.Skill;
     private const CardRarity rarity = CardRarity.Uncommon;
     private const TargetType targetType = TargetType.Self;
     private const bool shouldShowInCardLibrary = true;
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<MorbidPower>()];
-
-    protected override IEnumerable<DynamicVar> CanonicalVars =>
-    [
-        new BlockVar(4m, ValueProp.Move),
-    ];
-
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
     public ImSoDespair() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary) { }
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        // 获得格挡；本回合拥有病态的敌人对你造成的伤害减半
-        await CreatureCmd.GainBlock(base.Owner.Creature, base.DynamicVars.Block.BaseValue, base.DynamicVars.Block.Props, cardPlay);
-        await PowerCmd.Apply<ImSoDespairPower>(base.Owner.Creature, 1m, base.Owner.Creature, this);
+        // 丢弃所有手牌，然后抽取相同数量的牌
+        var piles = PileType.Hand.GetPile(base.Owner).Cards;
+        await CardCmd.DiscardAndDraw(choiceContext, piles, piles.Count());
     }
 
     protected override void OnUpgrade()
     {
-        base.DynamicVars.Block.UpgradeValueBy(2m);
+        base.RemoveKeyword(CardKeyword.Exhaust);
     }
 }
