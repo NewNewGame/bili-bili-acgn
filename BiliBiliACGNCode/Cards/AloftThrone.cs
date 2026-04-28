2d51a91b3b3d751a4b48662df04750995e50ace7
imwheat <62542063+imwheat@users.noreply.github.com>
2026-04-28 09:50:25 +0800

    update 重做高高在上

--- BiliBiliACGNCode/Cards/AloftThrone.cs
+++ BiliBiliACGNCode/Cards/AloftThrone.cs
@@ -2,12 +2,11 @@
 //* 文件：AloftThrone(高高在上)
 //* 作者：wheat
 //* 创建时间：2026/04/11
-//* 描述：能力：每当女儿发动进攻时，抽1张牌。
+//* 描述：失去1个充能球栏位。\n获得{Focus:diff()}点[gold]集中[/gold]。\n获得{Dexterity:diff()}点[gold]敏捷[/gold]。
 //*******************************************************
 
 using BaseLib.Utils;
 using BiliBiliACGN.BiliBiliACGNCode.Cards.CardPool;
-using BiliBiliACGN.BiliBiliACGNCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
@@ -28,12 +27,12 @@ public sealed class AloftThrone : CardBaseModel
 
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new DynamicVar("Strength", 2m),
+        new DynamicVar("Focus", 1m),
         new DynamicVar("Dexterity", 2m),
     ];
 
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [
-        HoverTipFactory.FromPower<StrengthPower>(),
+        HoverTipFactory.FromPower<FocusPower>(),
         HoverTipFactory.FromPower<DexterityPower>(),
     ];
 
@@ -45,14 +44,14 @@ public sealed class AloftThrone : CardBaseModel
         await CreatureCmd.TriggerAnim(base.Owner.Creature, "Cast", base.Owner.Character.CastAnimDelay);
         // 失去1个充能球栏位
         OrbCmd.RemoveSlots(base.Owner, 1);
-        // 获得力量和敏捷
-        await PowerCmd.Apply<StrengthPower>(base.Owner.Creature, base.DynamicVars["Strength"].BaseValue, base.Owner.Creature, this);
+        // 获得集中和敏捷
+        await PowerCmd.Apply<FocusPower>(base.Owner.Creature, base.DynamicVars["Focus"].BaseValue, base.Owner.Creature, this);
         await PowerCmd.Apply<DexterityPower>(base.Owner.Creature, base.DynamicVars["Dexterity"].BaseValue, base.Owner.Creature, this);
     }
 
     protected override void OnUpgrade()
     {
-        base.DynamicVars["Strength"].UpgradeValueBy(1m);
+        base.DynamicVars["Focus"].UpgradeValueBy(1m);
         base.DynamicVars["Dexterity"].UpgradeValueBy(1m);
     }
 }
