e11fb3c1a7b7a109790fbdd123286b936a00bef5
imwheat <62542063+imwheat@users.noreply.github.com>
2026-05-15 11:26:49 +0800

    fix 修复大家闺秀bug

--- BiliBiliACGNCode/Powers/NobleLadyPower.cs
+++ BiliBiliACGNCode/Powers/NobleLadyPower.cs
@@ -25,7 +25,6 @@ public sealed class NobleLadyPower : PowerBaseModel
     public override PowerType Type => PowerType.Buff;
 
     public override PowerStackType StackType => PowerStackType.Counter;
-    public override bool IsInstanced => true;
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(StaticHoverTip.Evoke), HoverTipFactory.Static(CustomeHoverTips.AttackOrb)];
     protected override IEnumerable<DynamicVar> CanonicalVars => [new StringVar("Applier")];
     public override Task AfterApplied(Creature? applier, CardModel? cardSource)
